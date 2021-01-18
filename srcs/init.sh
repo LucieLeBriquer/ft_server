f9d3cdd9d27d8186b910ea74a10873801069eb4a
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2021-01-18 17:11:53 +0100

    link php and database

--- srcs/init.sh
+++ srcs/init.sh
@@ -1,4 +1,4 @@
 service php7.3-fpm start
 service mysql start
-cat database.txt | mysql -uroot --skip-password && rm database.txt
+cat database.txt | mysql -uroot --password=password && rm database.txt
 service nginx start
