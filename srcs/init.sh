53bcd965300d883ee965cdfb3004d71c7f70b4d1
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2021-01-19 16:46:12 +0100

    all good

--- srcs/init.sh
+++ srcs/init.sh
@@ -1,5 +1,5 @@
 service php7.3-fpm start
 service mysql start
 cat database.sql | mysql -uroot --password=password
-cat /var/wordpress/phpmyadmin/sql/create_tables.sql | mysql -uroot --password=password
+cat /var/www/wordpress/phpmyadmin/sql/create_tables.sql | mysql -uroot --password=password
 service nginx start
